eebc058b040e7c8e23171a2b4a468049f08c8223
Ahmad Khaled Zaid <ahmadk.zaid97@gmail.com>
2022-06-25 16:40:46 +0300

    Solve the challenge

---
+++ Linked_List/zipper.py
@@ -0,0 +1,13 @@
+from Linked_List.linked_list import LinkedList
+
+list1 = LinkedList()
+list1.insert_last(1)
+list1.insert_last(2)
+list1.insert_last(3)
+
+list2 = LinkedList()
+list2.insert_last(4)
+list2.insert_last(5)
+list2.insert_last(6)
+# print(list1.to_string())
+
